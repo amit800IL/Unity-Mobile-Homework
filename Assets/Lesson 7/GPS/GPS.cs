b23cd80526a5bf6fd68a62763fa828fb30c64b56
amit800IL <108745954+amit800IL@users.noreply.github.com>
2022-08-27 19:41:54 +0300

    Lesson 7

---
+++ Assets/Lesson 7/GPS/GPS.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class GPS : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI Gpsstatus;
+    [SerializeField] TextMeshProUGUI latitudeValue;
+    [SerializeField] TextMeshProUGUI longitudeValue;
+    [SerializeField] TextMeshProUGUI altitudeValue;
+   
+
+
+    private void Start()
+    {
+        StartCoroutine(GPSloc());
+    }
+
+    IEnumerator GPSloc()
+    {
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        Input.location.Start();
+
+        int maxWait = 20;
+
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait < 1)
+        {
+            Gpsstatus.text = "Time Out";
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Gpsstatus.text = "Unable To Detrmine Device Location";
+            yield break;
+        }
+
+        else
+        {
+            Gpsstatus.text = "Running";
+            InvokeRepeating("UpdateGPSData", 0.5f, 1f);
+        }
+
+        
+    }
+        private void UpdateGPSData()
+        {
+          if (Input.location.status == LocationServiceStatus.Running)
+        {
+            Gpsstatus.text = "Running";
+            latitudeValue.text = Input.location.lastData.latitude.ToString();
+            longitudeValue.text = Input.location.lastData.longitude.ToString();
+            altitudeValue.text = Input.location.lastData.altitude.ToString();
+           
+
+        }
+
+        else
+        {
+            Gpsstatus.text = "Stop";
+        }
+
+        }
+}
---
+++ Assets/Lesson 7/Gyro/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    Quaternion origRotation;
+
+    private Gyroscope m_Gyroscope;
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+        m_Gyroscope = Input.gyro;
+
+
+        m_Gyroscope.enabled = true;
+        origRotation = m_Gyroscope.attitude;
+
+        count = 0;
+
+
+        SetCountText();
+
+
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //  Vector3 GyroVec = new Vector3(m_Gyroscope.userAcceleration.x, 0, m_Gyroscope.userAcceleration.z);
+        
+        Vector3 GyroVector = new Vector3(Input.acceleration.x * speed,0, Input.acceleration.z * -speed);
+
+        rb.velocity = GyroVector;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+
+            other.gameObject.SetActive(false);
+
+
+            count += 1;
+
+
+            SetCountText();
+        }
+    }
+
+
+    void SetCountText()
+    {
+
+        countText.text = "Count: " + count.ToString();
+
+
+        if (count >= 12)
+        {
+
+            winText.text = "You Win!";
+        }
+    }
+}
